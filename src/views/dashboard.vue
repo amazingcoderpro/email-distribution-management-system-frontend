6fd22ccd639437256a4b80cf536bd27743f745cc
ZJY <1219674323@qq.com>
2019-07-24 11:48:11 +0800

    添加db模块

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -7,52 +7,51 @@
         <div class="dataBox">
             <div class="dataBoxSon">
                 <div class="title">Total Revenue</div>
-                <div class="content">$535467.58</div>
+                <div class="content">${{echartData.topDashboard.total_revenue}}</div>
             </div>
             <div class="dataBoxSon">
-                <div class="title">Total Revenue</div>
-                <div class="content">$535467.58</div>
+                <div class="title">Total Orders</div>
+                <div class="content">${{echartData.topDashboard.total_orders}}</div>
             </div>
             <div class="dataBoxSon">
-                <div class="title">Total Revenue</div>
-                <div class="content">$535467.58</div>
+                <div class="title">Avg. Repeat Purchase Rate</div>
+                <div class="content">{{echartData.topDashboard.avg_repeat_purchase_rate}}%</div>
             </div>
             <div class="dataBoxSon">
-                <div class="title">Total Revenue</div>
-                <div class="content">$535467.58</div>
+                <div class="title">Avg. Conversion Rate</div>
+                <div class="content">{{echartData.topDashboard.avg_conversion_rate}}%</div>
             </div>
             <div class="dataBoxSon">
-                <div class="title">Total Revenue</div>
-                <div class="content">$535467.58</div>
+                <div class="title">Total Sent</div>
+                <div class="content">${{echartData.topDashboard.total_sent}}</div>
             </div>
             <div class="dataBoxSon">
-                <div class="title">Total Revenue</div>
-                <div class="content">$535467.58</div>
+                <div class="title">Avg. Open Rate</div>
+                <div class="content">{{echartData.topDashboard.avg_open_rate}}%</div>
             </div>
             <div class="dataBoxSon">
-                <div class="title">Total Revenue</div>
-                <div class="content">$535467.58</div>
+                <div class="title">Avg. Click Rate</div>
+                <div class="content">{{echartData.topDashboard.avg_click_rate}}%</div>
             </div>
             <div class="dataBoxSon">
-                <div class="title">Total Revenue</div>
-                <div class="content">$535467.58</div>
+                <div class="title">Avg. Unsubscribe Rate</div>
+                <div class="content">{{echartData.topDashboard.avg_unsubscribe_rate}}%</div>
             </div>
         </div>
         <el-form :inline="true" :model="echartData" class="demo-form-inline fromClass " label-width="100px">
-          <!-- Pinterest -->
-          <el-form-item>
-            <el-select placeholder="Pinterest" v-model="echartData.echartType" :class="'W200'">
-              <el-option v-for="(item,index) in echartData.echartTypeArray" :key="index" :label="item.title" :value="item.value"></el-option>
+            <el-form-item>
+            <el-select placeholder="Pinterest" v-model="echartData.echartType" :class="'W200'" @change="echartTypeChange">
+                <el-option v-for="(item,index) in echartData.echartTypeArray" :key="index" :label="item.title" :value="item.value"></el-option>
             </el-select>
-          </el-form-item>
-          <el-form-item class="FR">
+            </el-form-item>
+            <el-form-item class="FR">
             <el-date-picker type="daterange" v-model="echartData.echartTimeValue" range-separator="--" start-placeholder="start time" end-placeholder="End time" :disabled="echartData.echartTimeState=='1'"></el-date-picker>
-          </el-form-item>
-          <el-form-item class="FR">
+            </el-form-item>
+            <el-form-item class="FR">
             <el-select placeholder="Pinterest" v-model="echartData.echartTimeType" @change="timeTypeChange" :class="'W200'">
-              <el-option v-for="(item,index) in echartData.echartTimeTypeArray" :key="index" :label="item.label" :value="item.value"></el-option>
+                <el-option v-for="(item,index) in echartData.echartTimeTypeArray" :key="index" :label="item.label" :value="item.value"></el-option>
             </el-select>
-          </el-form-item>
+            </el-form-item>
         </el-form>
         <div :class="echartData.className" :id="echartData.id" :style="'height:500px;width:90%;'" ref="myEchart">
         </div>
@@ -71,26 +70,38 @@ export default {
                 id:"myEchart",
                 height:"90%",
                 width:"500px",
-                echartType:0, // 数据类型
-                echartTimeType:0, // 展示几天
+                echartType:"revenue", // 数据类型
+                bigArray:[],
+                topDashboard:{
+                    total_orders: 0,
+                    avg_repeat_purchase_rate: 0,
+                    avg_conversion_rate: 0,
+                    total_revenue: 0,
+                    total_sent: 0,
+                    avg_open_rate: 0,
+                    avg_click_rate: 0,
+                    avg_unsubscribe_rate: 0
+                },
+                echartTimeType:2, // 展示几天
                 echartTimeValue:[], // 时间戳
                 echartTimeState:'1',
                 echartTypeArray:[
-                    {title:'Revenue',value:0},
-                    {title:'Orders',value:1},
-                    {title:'Traffics',value:2},
-                    {title:'Sent',value:3},
-                    {title:'Opens',value:4},
-                    {title:'Clicks',value:5}
+                    {title:'Revenue',value:"revenue"},
+                    {title:'Orders',value:"orders"},
+                    // {title:'Traffics',value:2},
+                    {title:'Sent',value:"total_sent"},
+                    {title:'Opens',value:"total_open"},
+                    {title:'Clicks',value:"total_click"}
                 ],
                 echartTimeTypeArray:[
                     {"label":"Yesterday","value":0},
                     {"label":"Today","value":1},
-                    {"label":"近7天","value":2},
-                    {"label":"本月","value":3},
-                    {"label":"本年","value":4},
-                    {"label":"Custom","value":5},
+                    {"label":"Last 7 Days","value":2},
+                    {"label":"Current Month","value":3},
+                    {"label":"This Year","value":4},
+                    // {"label":"Custom","value":5},
                 ],
+
             },
             chart: null,
             Xvalue:['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
@@ -108,14 +119,49 @@ export default {
             }
         }
         this.timeTypeChange();
-        this.initChart();   
+        this.topInit();
     },
     methods:{
+        topInit(){
+            let urlString = `/api/v1/top_dashboard/`;
+            this.$axios.get(urlString).then(res => {
+               // this.loadingState.dashboardFive = false;
+                if(res.data.code==1){
+                    this.echartData.topDashboard = res.data.data;
+                    console.log(this.echartData.topDashboard)
+                }else{
+                    this.$message("Acquisition failure!");
+                }
+            })
+            .catch(error => {
+                // this.loadingState.dashboardFive = false;
+                this.$message("Interface timeout!");
+            });
+        },
+        init(){
+            let _star = base.dateFormat(this.echartData.echartTimeValue[0])
+            let _end = base.dateFormat(this.echartData.echartTimeValue[1])
+            let urlString = `/api/v1/bottom_dashboard/?begin_time=${_star}&end_time=${_end}`;
+            this.$axios.get(urlString).then(res => {
+               // this.loadingState.dashboardFive = false;
+                if(res.data.code==1){
+                    this.echartData.bigArray = res.data.data;
+                }else{
+                    this.$message("Acquisition failure!");
+                }
+                this.echartTypeChange();
+            })
+            .catch(error => {
+                // this.loadingState.dashboardFive = false;
+                this.$message("Interface timeout!");
+                this.echartTypeChange();
+            });
+        },
         initChart() {
             this.chart = echarts.init(this.$refs.myEchart);
             this.chart.setOption({
                 xAxis: {type: 'category',boundaryGap: false,data: this.Xvalue},
-                grid:{top:"50px",left:"50px",right:"15px",bottom:"50px"},
+                grid:{top:"50px",left:"50px",right:"20px",bottom:"50px"},
                 tooltip: {trigger: 'axis'},
                 yAxis: {type: 'value',min: 0,},
                 series: [{data:  this.Yvalue,type: 'line',areaStyle: {},color:['#0065a3'], smooth:true,symbolSize: 12, }]
@@ -130,30 +176,42 @@ export default {
                 var _end;
                 if(this.echartData.echartTimeType == 0){
                     // 昨天
-                     _star = new Date(base.dateFormat(new Date(new Date().getTime()-1000*24*60*60),"day") + " 00:00:00");
-                     _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 00:00:00");
+                    _star = new Date(base.dateFormat(new Date(new Date().getTime()-1000*24*60*60),"day") + " 00:00:00");
+                    _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 23:59:59");
                 }else if(this.echartData.echartTimeType == 1){
                     // 今天
-                     _star = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 00:00:00");
-                     _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 00:00:00");
+                    _star = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 00:00:00");
+                    _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 23:59:59");
                 }else if(this.echartData.echartTimeType == 2){
                     // 近七天
-                     _star = new Date(base.dateFormat(new Date(new Date().getTime()-6*1000*24*60*60),"day") + " 00:00:00");
-                     _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 00:00:00");
+                    _star = new Date(base.dateFormat(new Date(new Date().getTime()-6*1000*24*60*60),"day") + " 00:00:00");
+                    _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 23:59:59");
                 }else if(this.echartData.echartTimeType ==3){
                     // 本月
                     var time = new Date();
-                     _star = new Date(base.dateFormat(time.getFullYear()+"-"+ (time.getMonth()+1) +"-1"+ " 00:00:00"));
-                     _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 00:00:00");
+                    _star = new Date(base.dateFormat(time.getFullYear()+"-"+ (time.getMonth()+1) +"-1"+ " 00:00:00"));
+                    _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 23:59:59");
                 }else if(this.echartData.echartTimeType ==4){
                     //本年度
                     var time = new Date();
-                     _star = new Date(base.dateFormat(time.getFullYear()+"-1-1"+ " 00:00:00"));
-                     _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 00:00:00");
+                    _star = new Date(base.dateFormat(time.getFullYear()+"-1-1"+ " 00:00:00"));
+                    _end = new Date(base.dateFormat(new Date(new Date().getTime()),"day") + " 23:59:59");
                 }
                 this.echartData.echartTimeValue = [_star,_end]
             }
-
+            this.init();
+        },
+        echartTypeChange(){
+            // echartData.echartType
+            // Yvalue\
+            this.Xvalue = [];
+            this.Yvalue = [];
+            // console.log(this.echartData.bigArray)
+            this.echartData.bigArray.map(e => {
+                this.Xvalue.unshift(base.dateFormat(e.create_time,"noyear"));
+                this.Yvalue.unshift(e[this.echartData.echartType]);
+            });
+            this.initChart();
         }
     },
     beforeDestroy() {
